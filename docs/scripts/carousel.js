97b436bb1f7972aa68a58500577a11456d1ac5bb
Sergey Safronov <s.safronov@korda-group.ru>
2023-08-17 11:01:33 +0200

    Add other blocks

--- docs/scripts/carousel.js
+++ docs/scripts/carousel.js
@@ -1,17 +1,18 @@
-const carouselCardMargin = 380;
-let currentSlide = 1;
-const container = document.querySelector('.carousel');
+const containers = document.querySelectorAll('.carousel-content');
+let container = containers[0];
+
+function selectCarousel(a) {
+  container = containers[a];
+}
 
 function doForwardSlide() {
-  const newFirstCard = document.querySelector('.carousel-card:first-child');
-  newFirstCard.style.marginLeft = carouselCardMargin + 'px';
+  const newFirstCard = container.querySelector('.carousel-card:first-child');
   container.insertBefore(newFirstCard, container.lastChild);
   newFirstCard.style.marginLeft = '0';
 }
 
 function doBackSlide() {
-  const newFirstCard = document.querySelector('.carousel-card:last-child');
-  newFirstCard.style.marginRight = carouselCardMargin + 'px';
+  const newFirstCard = container.querySelector('.carousel-card:last-child');
   container.insertBefore(newFirstCard, container.firstChild);
   newFirstCard.style.marginRight = '0';
 }
